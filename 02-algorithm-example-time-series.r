68bd11c6e3bb052414200179d996979b7f3aea6b
Jerry Overton <jerryaoverton@yahoo.com>
2017-09-22 10:09:40 -0500

    Add files via upload

---
+++ 02-algorithm-example-time-series.r
@@ -0,0 +1,31 @@
+#Hypothesis: Close prices for the S&P 500 follows a rythmic, predictable pattern
+
+library(prophet)
+library(dplyr)
+
+#Read, from Yahoo! Finance, the S&P 500 Index data for the last month
+urlfile <- "http://chart.finance.yahoo.com/table.csv?s=^GSPC&a=1&b=28&c=2016&d=1&e=28&f=2017&g=d&ignore=.csv"
+data <- read.csv(urlfile)
+head(data)
+
+#Read the close price for the the S&P 500 Indes for the last month
+cols <- c("Date", "Close")
+
+#Create the data set needed to fit a model
+spx_history <- data[,cols]
+names(spx_history)[names(spx_history) == 'Date'] <- 'ds'
+names(spx_history)[names(spx_history) == 'Close'] <- 'y'
+
+head(spx_history)
+
+#Fit a time-series model
+model <- prophet(spx_history)
+
+#Make a prediction
+future <- make_future_dataframe(model, periods = 90)
+forecast <- predict(model, future)
+plot(model, forecast)
+
+#EXERCISE: How plausible is the hypothesis given the evidence?
+
+#EXERCISE: Take the algorithm above and hack it. Propose a new hypothesis and extend this algorithm to investigate.
